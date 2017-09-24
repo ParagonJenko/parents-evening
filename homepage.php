9377395c693f40392bd09d9584a9660d83440fd2
Alex Jenkinson <thetechnicleg@gmail.com>
2017-09-24 21:58:31 +0100

    git ignore added + session data

--- homepage.php
+++ homepage.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 // Allows session variables to be used.
 session_start();
 
 // Includes the database configuration file.
-require($_SERVER['DOCUMENT_ROOT']."/parents-evening/server/config.php");  
+require($_SERVER['DOCUMENT_ROOT']."/parents-evening/server/config.php");
+require($_SERVER['DOCUMENT_ROOT'].DOCROOT."scripts/core-site/session/session_active.php"); 
 ?>
 
 <!-- TEMPLATE DESIGNED BY ALEX JENKINSON -->
@@ -16,10 +17,10 @@ require($_SERVER['DOCUMENT_ROOT']."/parents-evening/server/config.php");
 	<?php $pagetitle = "Homepage"; require($_SERVER['DOCUMENT_ROOT'].DOCROOT."includes/head.php"); ?>
 
 	<body class="text-center">
-	
+
 	<!-- Require navbar from specified file -->
 	<?php require($_SERVER['DOCUMENT_ROOT'].DOCROOT."includes/navbar.php"); ?>
-	
+
 	<?php require($_SERVER['DOCUMENT_ROOT'].DOCROOT."includes/errormessage.php"); ?>
 
 	<h1>Welcome to <?php $sql = "SELECT * FROM school_data WHERE id = {$_SESSION['school_id']}"; $result = mysqli_query($conn, $sql); $row = mysqli_fetch_assoc($result); echo $row['school_name'] . " "; ?>Parents Evening Web System</h1>
@@ -27,7 +28,7 @@ require($_SERVER['DOCUMENT_ROOT']."/parents-evening/server/config.php");
 
 	<!-- Require footer from specified file -->
 	<?php require($_SERVER['DOCUMENT_ROOT'].DOCROOT."includes/footer.php"); ?>
-	
+
 	</body>
 </html>
 
