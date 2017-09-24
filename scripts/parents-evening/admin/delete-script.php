ddcc7b2526565b1bb4741b6c8fceb68e2d473c5e
Alex Jenkinson <thetechnicleg@gmail.com>
2017-09-24 11:00:20 +0100

    Redesign to focus on teacher schedule instead of classes.

--- scripts/parents-evening/admin/delete-script.php
+++ scripts/parents-evening/admin/delete-script.php
@@ -6,17 +6,42 @@ require($_SERVER['DOCUMENT_ROOT'].'/parents-evening/server/config.php'); //Chang
 
 $header_URL = "Location: ".WEBURL.DOCROOT."pages/parents-evening/admin/";
 
+// Set serverlog variables
+$ipaddress = $_SERVER['REMOTE_ADDR'];
+$user = $_SESSION['username'];
+$location = "delete-script.php";
+
 $sql = "DELETE FROM {$_GET['table_name']} WHERE id = {$_POST['delete_id']}";
 
-echo $sql;
-
-/*if(mysqli_query($conn, $sql))
+if(mysqli_query($conn, $sql))
 {
 	// Success
+	// Insert record of this action into serverlog
+	$action = "$table has been deleted at ID: $id";
+	$sql_serverlog = "INSERT INTO server_log (ip_address, user, action, location) VALUES ('$ipaddress', '$user', '$action', '$location')";
+	mysqli_query($conn, $sql_serverlog);
+
+	// Closes the database connection
+	mysqli_close($conn);
+	// Sets the redirect location
+	header($header_URL);
+	// Exits the script
+	exit();
 }
 else
 {
 	// Fail
-}*/
+	// Insert record of this action into serverlog
+	$action = "$table has been failed to be deleted at ID: $id";
+	$sql_serverlog = "INSERT INTO server_log (ip_address, user, action, location) VALUES ('$ipaddress', '$user', '$action', '$location')";
+	mysqli_query($conn, $sql_serverlog);
+
+	// Closes the database connection
+	mysqli_close($conn);
+	// Sets the redirect location
+	header($header_URL);
+	// Exits the script
+	exit();
+}
 
 ?>
--- scripts/parents-evening/admin/reset-password.php
+++ scripts/parents-evening/admin/reset-password.php
@@ -9,44 +9,28 @@ $header_URL = "Location: ".WEBURL.DOCROOT."pages/parents-evening/admin/";
 // Set serverlog variables
 $ipaddress = $_SERVER['REMOTE_ADDR'];
 $user = $_SESSION['username'];
-$location = "add-script.php";
-
-$table = $_GET['table_name'];
+$location = "reset-password-script.php";
 
 $password_string = '!@#$%*&abcdefghijklmnpqrstuwxyzABCDEFGHJKLMNPQRSTUWXYZ23456789';
 $password_random = substr(str_shuffle($password_string), 0, 12);
+$password_hash = password_hash($password_random, PASSWORD_DEFAULT);
 
-$password_hash = password_hash("1234", PASSWORD_DEFAULT);
+$sql = "UPDATE users SET `password` = '$password_hash' WHERE id = {$_POST['id']}";
 
-switch($table)
-{
-	case "users":
-		$columns = "status, forename, surname, username, email_address, school_id, password";
-		$values = "'{$_POST['status']}', '{$_POST['forename']}', '{$_POST['surname']}', '{$_POST['username']}', '{$_POST['email_address']}', {$_SESSION['school_id']}, '{$password_hash}'";
-		break;
-	case "parents_evenings":
-		$columns = "school_id, evening_date, start_time, end_time";
-		$values = "{$_SESSION['school_id']}, '{$_POST['evening_date']}', '{$_POST['start_time']}', '{$_POST['end_time']}'";
-		break;
-	default:
-		echo "FAIL";
-		exit();
-}
-
-$sql = "INSERT INTO $table ($columns) VALUES ($values)";
+echo $sql;
 
 if(mysqli_query($conn, $sql))
 {
 	// Success
 	// Insert record of this action into serverlog
-	$action = "$table has been added at ID: $id";
+	$action = "Reset Password by $user for ID: $id";
 	$sql_serverlog = "INSERT INTO server_log (ip_address, user, action, location) VALUES ('$ipaddress', '$user', '$action', '$location')";
 	mysqli_query($conn, $sql_serverlog);
 
 	// Closes the database connection
 	mysqli_close($conn);
 	// Sets the redirect location
-	header($header_URL);
+	header($header_URL."?error=7&password_reset={$password_random}");
 	// Exits the script
 	exit();
 }
@@ -54,15 +38,16 @@ else
 {
 	// Fail
 	// Insert record of this action into serverlog
-	$action = "$table has been failed to be added at ID: $id";
+	$action = "Reset Password failed by $user for ID: $id";
 	$sql_serverlog = "INSERT INTO server_log (ip_address, user, action, location) VALUES ('$ipaddress', '$user', '$action', '$location')";
 	mysqli_query($conn, $sql_serverlog);
 
 	// Closes the database connection
 	mysqli_close($conn);
 	// Sets the redirect location
-	header($header_URL);
+	header($header_URL."?error=2");
 	// Exits the script
 	exit();
 }
+
 ?>
