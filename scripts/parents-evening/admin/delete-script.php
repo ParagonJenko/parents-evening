78c2f807a48e46aa3697a30e6eb236c3e4ff6410
Alex Jenkinson <alex.jenkinson@paragon-gaming.co.ukgit config --global user.email alex.jenkinson@paragon-gaming.co.uk>
2017-10-02 22:53:06 +0100

    Changed ID variable to utilise GET command

--- scripts/parents-evening/admin/delete-script.php
+++ scripts/parents-evening/admin/delete-script.php
@@ -4,7 +4,15 @@ session_start();
 // Includes the database configuration file.
 require($_SERVER['DOCUMENT_ROOT'].'/parents-evening/server/config.php'); //Change to where it is stored in your website.
 
-$header_URL = "Location: ".WEBURL.DOCROOT."pages/parents-evening/admin/";
+if($_SESSION['status'] == "teacher")
+{
+	$suffix = "teacher";
+}
+else
+{
+	$suffix = "admin";
+}
+$header_URL = "Location: ".WEBURL.DOCROOT."pages/parents-evening/$suffix/";
 
 // Set serverlog variables
 $ipaddress = $_SERVER['REMOTE_ADDR'];
@@ -12,7 +20,7 @@ $user = $_SESSION['username'];
 $location = "delete-script.php";
 $table = $_GET['table_name'];
 
-$sql = "DELETE FROM {$table} WHERE id = {$_POST['delete_id']}";
+$sql = "DELETE FROM {$table} WHERE id = {$_POST['delete_id']}{$_GET['delete_id']}";
 
 if(mysqli_query($conn, $sql))
 {
